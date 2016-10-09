8f524e8a6fef34f92f251698f7faf92d2596b26f
JyotendraPallav <Jyotendra@gmail.com>
2016-10-09 23:45:15 +0530

    First commit

---
+++ app/src/main/java/com/example/a0262/coolcalc/Activity_calc.java
@@ -0,0 +1,13 @@
+package com.example.a0262.coolcalc;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Activity_calc extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calc);
+    }
+}
